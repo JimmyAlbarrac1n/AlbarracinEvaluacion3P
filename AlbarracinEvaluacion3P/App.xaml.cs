86e4c139f218dce5987c4017b81aa1ca84d6dc48
JimmyAlbarrac1n <jimmismael2003@gmail.com>
2024-07-10 10:36:34 -0500

    Agregar archivos de proyecto.

---
+++ AlbarracinEvaluacion3P/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AlbarracinEvaluacion3P
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AlbarracinEvaluacion3P/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AlbarracinEvaluacion3P
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
